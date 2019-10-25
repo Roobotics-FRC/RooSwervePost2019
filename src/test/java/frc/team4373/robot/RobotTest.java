6997252bde7c58e8c8c15fb82a10b9f1a606f9e1
Aria <23ariag@abingtonfriends.net>
2019-10-25 16:23:54 -0400

    Add assertEquals test

--- src/test/java/frc/team4373/robot/RobotTest.java
+++ src/test/java/frc/team4373/robot/RobotTest.java
@@ -11,5 +11,17 @@ class RobotTest {
 
     @Test
     void constrainPercentOutput() {
+        assertEquals(Robot.constrainPercentOutput(30), 1);
+        assertEquals(Robot.constrainPercentOutput(1.1), 1);
+        assertEquals(Robot.constrainPercentOutput(-30), -1);
+        assertEquals(Robot.constrainPercentOutput(-1.1), -1);
+        assertEquals(Robot.constrainPercentOutput(0), 0);
+        assertEquals(Robot.constrainPercentOutput(0.2), 0.2);
+        assertEquals(Robot.constrainPercentOutput(-0.2), -0.2);
+        assertEquals(Robot.constrainPercentOutput(0.9), 0.9);
+        assertEquals(Robot.constrainPercentOutput(-0.9), -0.9);
+        assertEquals(Robot.constrainPercentOutput(100), 1) ;
+        assertEquals(Robot.constrainPercentOutput(-100), -1) ;
+        assertEquals(Robot.constrainPercentOutput(0.25), 0.25);
     }
 }
